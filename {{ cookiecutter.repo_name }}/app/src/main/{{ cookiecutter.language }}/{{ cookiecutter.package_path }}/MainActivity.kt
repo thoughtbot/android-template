a1ff2b492aba4a822504d51f9a263f294b1f01d6
Amanda Hill <amandabesshill@gmail.com>
2017-02-10 11:33:42 -0800

    Add place holder files so all directories are created

---
+++ {{ cookiecutter.repo_name }}/app/src/main/{{ cookiecutter.language }}/{{ cookiecutter.package_path }}/MainActivity.kt
@@ -0,0 +1,8 @@
+package {{ cookiecutter.package_name }}
+
+import android.support.v7.app.AppCompatActivity
+
+/*
+ * This class is just a place holder so all the package directories are created.
+ */
+class MainActivity : AppCompatActivity()
