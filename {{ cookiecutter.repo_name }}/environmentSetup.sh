62355df6073408ca8ef3a97bd9e96657a80791c2
Amanda Hill <amandabesshill@gmail.com>
2017-02-10 10:43:23 -0800

    Update CI script

--- {{ cookiecutter.repo_name }}/environmentSetup.sh
+++ {{ cookiecutter.repo_name }}/environmentSetup.sh
@@ -4,28 +4,31 @@ function copy_env_vars_to_gradle_properties {
     GRADLE_PROPERTIES=$HOME"/.gradle/gradle.properties"
     export GRADLE_PROPERTIES
 
-    // echo "Writing API_CLIENT_ID to gradle.properties..."
-    //echo "API_CLIENT_ID=$API_CLIENT_ID" >> "$GRADLE_PROPERTIES"
+    if [ ! -f "$GRADLE_PROPERTIES" ]; then
+      echo "Gradle Properties does not exist"
 
+      echo "Creating Gradle Properties directory and file..."
+      mkdir -p "$HOME/.gradle/"
+      touch "$GRADLE_PROPERTIES"
+
+      // echo "Writing API_CLIENT_ID to gradle.properties..."
+      //echo "API_CLIENT_ID=$API_CLIENT_ID" >> "$GRADLE_PROPERTIES"
+    fi
 }
 
 function affirmative_android_update {
   echo y | android update sdk --no-ui --all --filter "$1"
 }
 
-function get_android_sdk {
+function get_android_sdk_25 {
   # fix the CircleCI path
   # export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
   DEPS="$ANDROID_HOME/installed-dependencies"
 
   if [ ! -e $DEPS ]; then
-    echo Updating SDK
-    echo y | android update sdk --no-ui --all --filter tool,extra-android-m2repository,extra-android-support,extra-google-google_play_services,extra-google-m2repository
-    # get the newest build tools
-    echo y | android update sdk --no-ui --all --filter build-tools-23.0.3
-    # create avd of our min sdk version for connected testing
-    echo no | android create avd -n testAVD -f -t android-19 --abi default/armeabi-v7a
+    echo Fetch and install Android SDK 25
+    echo y | android update sdk --no-ui --all --filter tools,platform-tools,build-tools-25.0.2,android-25,extra-google-m2repository,extra-google-google_play_services,extra-android-m2repository,extra-android-support
     # create the folder so we won't do this again (this is soooo Apache Ant right here)
     touch $DEPS
   fi
